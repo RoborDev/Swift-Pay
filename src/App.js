a54e947696ecdc567c5fef36ffc2ddf3011f325b
RoborDev <esioleerobor@gmail.com>
2025-01-07 20:01:06 +0100

    helmet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Helmet } from "react-helmet";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import Accounts from "./components/SideBar/pages/Accounts";
 import CardSc from "./components/SideBar/pages/CardSc";
 import Analytics from "./components/SideBar/pages/Analytics";
@@ -9,28 +9,26 @@ import BankDetailsCard from "./components/SideBar/Card/SideCard";
 
 function App() {
   return (
-    <Router>
-      <Helmet>
-        {/* Set the status bar color to white */}
-        <meta name="theme-color" content="#ffffff" />
-        <style>{`body { background-color: white; }`}</style>
-      </Helmet>
-      <div className="app">
-        {/* Main content area for pages */}
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Accounts />} />
-            <Route path="/accounts" element={<Accounts />} />
-            <Route path="/card" element={<CardSc />} />
-            <Route path="/analytics" element={<Analytics />} />
-            <Route path="/settings" element={<Settings />} />
-
-            {/* Route for BankDetailsCard */}
-            <Route path="/card/details" element={<BankDetailsCard />} />
-          </Routes>
+    <HelmetProvider>
+      <Router>
+        <Helmet>
+          <meta name="theme-color" content="#ffffff" />
+          <style>{`body { background-color: white; }`}</style>
+        </Helmet>
+        <div className="app">
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<Accounts />} />
+              <Route path="/accounts" element={<Accounts />} />
+              <Route path="/card" element={<CardSc />} />
+              <Route path="/analytics" element={<Analytics />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/card/details" element={<BankDetailsCard />} />
+            </Routes>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </HelmetProvider>
   );
 }
 
