cd42898fe9fc3b9ba2f51f05d4d817a821b6bea9
RoborDev <esioleerobor@gmail.com>
2025-01-08 12:53:07 +0100

    last screen size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { HelmetProvider, Helmet } from "react-helmet-async";
 import Accounts from "./components/SideBar/pages/Accounts";
 import CardSc from "./components/SideBar/pages/CardSc";
 import Analytics from "./components/SideBar/pages/Analytics";
@@ -9,34 +8,23 @@ import BankDetailsCard from "./components/SideBar/Card/SideCard";
 
 function App() {
   return (
-    <HelmetProvider>
-      <Router>
-        <Helmet>
-          {/* Theme Color for Android */}
-          <meta name="theme-color" content="#FF5733" />
-          {/* Enable web app mode on iOS */}
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="black-translucent"
-          />
-          {/* Body background for visual consistency */}
-          <style>{`body { background-color: #FF5733; }`}</style>
-        </Helmet>
-        <div className="app">
-          <div className="content">
-            <Routes>
-              <Route path="/" element={<Accounts />} />
-              <Route path="/accounts" element={<Accounts />} />
-              <Route path="/card" element={<CardSc />} />
-              <Route path="/analytics" element={<Analytics />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/card/details" element={<BankDetailsCard />} />
-            </Routes>
-          </div>
+    <Router>
+      <div className="app">
+        {/* Main content area for pages */}
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Accounts />} />
+            <Route path="/accounts" element={<Accounts />} />
+            <Route path="/card" element={<CardSc />} />
+            <Route path="/analytics" element={<Analytics />} />
+            <Route path="/settings" element={<Settings />} />
+
+            {/* Route for BankDetailsCard */}
+            <Route path="/card/details" element={<BankDetailsCard />} />
+          </Routes>
         </div>
-      </Router>
-    </HelmetProvider>
+      </div>
+    </Router>
   );
 }
 
